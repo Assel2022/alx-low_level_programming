520beb02a41ea0d62007c49f380b76dc69c62893
Assel2022 <101358877+Assel2022@users.noreply.github.com>
2022-04-07 14:21:01 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
+{#include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Prints out the last part of a quote in the standard error.
  *
@@ -10,4 +9,4 @@ int main(void)
 {
 	write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
-}
+}}
