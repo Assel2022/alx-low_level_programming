7cad8171f1581dee8ac94372266c64c675ccc769
Assel Kebede <asselmeseret@gmail.com>
2022-04-12 06:39:30 -0700

    Functions nested loops - Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,26 @@
-      for (i = 0; i < 9; i++)
-        {
-                _putchar(c[i]);
-        }
-        _putchar('\n');
-        return (0);
+#include <stdio.h>
+ 
+void print(long n)
+{
+    // If number is smaller than 0, put a - sign
+    // and change number to positive
+    if (n < 0) {
+        putchar('-');
+        n = -n;
+    }
+ 
+    // Remove the last digit and recur
+    if (n/10)
+        print(n/10);
+ 
+    // Print the last digit
+    putchar(n%10 + '0');
 }
-
+ 
+// Driver program to test above function
+int main()
+{
+    long int n = 12045;
+    print(n);
+    return 0;
+}
