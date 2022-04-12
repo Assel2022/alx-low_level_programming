083f5faeee1f9ab3f29426fd4046c668f66c6dbb
Assel Kebede <asselmeseret@gmail.com>
2022-04-12 06:26:11 -0700

    Functions nested loops - Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,8 @@
-#include "holberton.h"
-/**
- * main - prints the string "Holberton" from a character array.
- * Return: 0 on success.
- */
-int main(void)
-{
-	char c[] = "Holberton";
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(c[i]);
-	}
-	_putchar('\n');
-	return (0);
+      for (i = 0; i < 9; i++)
+        {
+                _putchar(c[i]);
+        }
+        _putchar('\n');
+        return (0);
 }
+
