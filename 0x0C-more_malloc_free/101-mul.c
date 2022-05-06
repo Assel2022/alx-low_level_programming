d2106c565f7cb738d39521f4b0492eec80f7dba6
Assel Kebede <asselmeseret@gmail.com>
2022-05-06 14:14:42 -0700

    Fixed?

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,14 +1,61 @@
+#include <stdlib.h>
 #include "main.h"
 
+/**
+ * _calloc - allocate (`size' * `nmemb') bytes and set to 0
+ * @nmemb: number of elements
+ * @size: number of bytes per element
+ *
+ * Return: pointer to memory, or NULL if `nmemb' or `size' is 0 or malloc fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *p;
+
+	if (size == 0 || nmemb == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; ++i)
+		p[i] = 0;
+	return (p);
+}
 
 /**
- * main - Entry point
+ * _strdigit - check if string `s' is composed only of digits
+ * @s: string to check
  *
- * Return: Always 0 (Success)
+ * Return: 1 if true, 0 if false
  */
-int main(void)
+int _strdigit(char *s)
+{
+	if (*s == '-' || *s == '+')
+		++s;
+	while (*s)
 	{
-		printf("Hel, World, World, Hel.\n");
-
-		return (0);
+		if (*s < '0' || *s > '9')
+		{
+			return (0);
+		}
+		++s;
 	}
+	return (1);
+}
+
+/**
+ * _puts - print string `s'
+ * @s: string to print
+ */
+void _puts(char *s)
+{
+	while (*s)
+		_putchar(*(s++));
+}
+
+/**
+ * rev_num_str - reverse a number string up to trailing zeros
+ * @start: beginning of number
+ * @end: end of number
+
