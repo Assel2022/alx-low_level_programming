11e8f0cc00fb5645212acb7167d3a36ee962bd09
Assel Kebede <asselmeseret@gmail.com>
2022-05-23 09:40:45 -0700

    New

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,25 +1,54 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
-* print_times_table - printing
-* @n: n
-*/
-n=5;
-int row;
-int col;
-int table [row][col];
-int i,j;
-for (i = 0; i<=15; i++)
+  * print_times_table- prnits the n table
+  *@n: the times table to be printed
+  *
+  * Return: void
+  */
+void print_times_table(int n)
 {
-for (j=0; j<=15; j++)
+int product;
+int x;
+int y;
+
+if (n >= 0 && n <= 15)
 {
-table [i][j]= i*j;
+	for (x = 0; x <= n; x++)
+	{
+		for (y = 0; y <= n; y++)
+		{
+		product = x * y;
+		if (y == 0)
+		{
+			_putchar('0');
+		}
+		else if (y != 0 && product < 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(product + '0');
+		}
+		else if (product < 100)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(product / 10 + '0');
+			_putchar(product % 10 + '0');
+		}
+		else if (product > 99)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(((product / 10)) / 10 + '0');
+			_putchar((product / 10) % 10 + '0');
+			_putchar(product % 10 + '0');
+		}
+		}
+		_putchar('\n');
 }
-}
-for(i = 0; i<=15; i++)
-{
-for(j=0; j<=15; j++)
-{
-printf("%d%",table[i][j]);
 }
 }
