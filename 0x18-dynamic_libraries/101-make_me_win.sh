bc9955cd52787f94039ccfb2abb45d18ac3a9528
Assel Kebede <asselmeseret@gmail.com>
2022-07-13 14:08:21 -0700

    Liball

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -q -O /tmp/liball.so "https://raw.githubusercontent.com/Assel2020/alx-low_level_programming/master/0x18-dynamic_libraries/liball.so"
-export LD_PRELOAD=/tmp/liball.so
-
+ wget -q -O /tmp/liball.so https://github.com/Assel2022/BatchScripts/raw/main/liball.so
+ export LD_PRELOAD=/tmp/liball.so
